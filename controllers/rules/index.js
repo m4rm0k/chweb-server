f3b00a988d2007b4830ccb574bf295e5f9024ab8
Rob <robert.carr@owasp.org>
2019-06-11 20:01:49 +0100

    Add rules controller

---
+++ controllers/rules/index.js
@@ -0,0 +1,127 @@
+const express = require('express')
+const verifyHost = require('>/controllers/host').middleware.authenticate
+const verifyUser = require('>/controllers/user').middleware.authenticate
+
+const Rule = require('>/models/Rule')
+const Setting = require('>/models/Setting')
+
+async function getRules (req, res, next) {
+  try {
+    const rules = await Rule.all()
+    const defaultAction = await Setting.find('defaultAction')
+
+    res.send({
+      success: true,
+      data: {
+        defaultAction: defaultAction.value,
+        rules: rules.map(r => ({
+          id: r.id.toString(),
+          type: r.type,
+          action: r.action,
+          host: r.host
+        }))
+      }
+    })
+  } catch (e) {
+    next(e)
+  }
+}
+
+async function createRule (req, res, next) {
+  try {
+    const body = req.body
+    if (!body.action || !body.host || !body.type) {
+      return res.status(400).send({
+        success: false
+      })
+    }
+
+    const rule = new Rule()
+    rule.action = body.action
+    rule.host = body.host
+    rule.type = body.type
+    await rule.save()
+
+    res.send({
+      success: true,
+      data: rule
+    })
+  } catch (e) {
+    next(e)
+  }
+}
+
+async function deleteRule (req, res, next) {
+  if (!req.query.id) {
+    return res.status(400).send({
+      success: false
+    })
+  }
+
+  try {
+    return res.send({
+      success: await Rule.delete(req.query.id)
+    })
+  } catch (e) {
+    return next(e)
+  }
+}
+
+async function updateRules (req, res, next) {
+  try {
+    let rules
+
+    if (Array.isArray(req.body)) {
+      rules = req.body
+    } else {
+      rules = [req.body]
+    }
+
+    const updatedRules = []
+    for (let i = 0; i < rules.length; i++) {
+      const rule = new Rule()
+      rule.id = rules[i].id
+      rule.action = rules[i].action
+      rule.type = rules[i].type
+      rule.host = rules[i].host
+
+      const isInvalid = !rule.id ||
+                        !rule.action ||
+                        !rule.type ||
+                        !rule.host
+
+      if (isInvalid) {
+        return res.status(400).send({
+          success: false
+        })
+      }
+
+      await rule.save()
+
+      updatedRules.push(rule)
+    }
+
+    return res.send({
+      success: true,
+      data: updatedRules
+    })
+  } catch (e) {
+    return next(e)
+  }
+}
+
+function bind (app) {
+  const router = express.Router()
+
+  router.route('/')
+    .get(verifyHost, getRules)
+    .put(verifyUser, createRule)
+    .post(verifyUser, updateRules)
+    .delete(verifyUser, deleteRule)
+
+  app.use('/api/v1/rules', router)
+}
+
+module.exports = {
+  bind: bind
+}
---
+++ controllers/rules/index.spec.js
@@ -0,0 +1,496 @@
+const expressHelper = require('>/test/support/express_helper')
+const request = require('supertest')
+const subject = require('./index')
+
+const Connection = require('>/lib/database/Connection')
+const Host = require('>/models/Host')
+const ObjectID = require('mongodb').ObjectID
+const User = require('>/models/User')
+
+describe('controllers/rules', () => {
+  const endpoint = '/api/v1/rules'
+  const rulesToInsert = [{
+    _id: new ObjectID(),
+    type: 'ACCESS',
+    action: 'ALLOW',
+    host: 'rastating.github.io'
+  },
+  {
+    _id: new ObjectID(),
+    type: 'ACCESS',
+    action: 'ALLOW',
+    host: '*.google.com'
+  },
+  {
+    _id: new ObjectID(),
+    type: 'ACCESS',
+    action: 'ALLOW',
+    host: 'r*ting.com'
+  }]
+
+  let rules
+  let connection
+  let app, agent
+  let host, user
+
+  beforeAll(async () => {
+    connection = await Connection.connect()
+
+    await connection.database.collection('rules').removeMany({})
+    await connection.database.collection('hosts').removeMany({})
+    await connection.database.collection('users').removeMany({})
+
+    app = expressHelper.create()
+    subject.bind(app)
+
+    host = new Host()
+    host.name = 'Test'
+    host.apiKey = new ObjectID().toString()
+    await host.save()
+
+    user = new User()
+    user.username = 'test'
+    user.apiKey = new ObjectID().toString()
+    await user.save()
+
+    agent = request.agent(app)
+  })
+
+  afterAll(async () => {
+    await connection.database.collection('rules').removeMany({})
+    await connection.database.collection('hosts').removeMany({})
+    await connection.database.collection('users').removeMany({})
+    await connection.client.close()
+  })
+
+  beforeEach(async () => {
+    rules = []
+
+    await connection.database
+      .collection('rules')
+      .insertMany(rulesToInsert)
+
+    await connection.database
+      .collection('settings')
+      .updateOne({
+        key: 'defaultAction'
+      }, {
+        $set: {
+          key: 'defaultAction',
+          value: 'REJECT'
+        }
+      }, {
+        upsert: true
+      })
+
+    for (let i = 0; i < rulesToInsert.length; i++) {
+      const rule = {
+        id: rulesToInsert[i]._id.toString(),
+        action: rulesToInsert[i].action,
+        type: rulesToInsert[i].type,
+        host: rulesToInsert[i].host
+      }
+
+      rules.push(rule)
+    }
+  })
+
+  afterEach(async () => {
+    await connection.database.collection('rules').removeMany({})
+  })
+
+  describe('GET /api/v1/rules', () => {
+    it('should require a valid host API key', async () => {
+      let res = await agent
+        .get(endpoint)
+        .query({ apiKey: host.apiKey })
+
+      expect(res.status).toBe(200)
+
+      res = await agent
+        .get(endpoint)
+        .query({ apiKey: 'invalid' })
+
+      expect(res.status).toBe(401)
+    })
+
+    it('should return the current ruleset as an array in `data.rules`', async () => {
+      const res = await agent
+        .get(endpoint)
+        .query({ apiKey: host.apiKey })
+
+      expect(res.body.data.rules).toEqual(rules)
+    })
+
+    it('should return the default action in `data.defaultAction`', async () => {
+      const res = await agent
+        .get(endpoint)
+        .query({ apiKey: host.apiKey })
+
+      expect(res.body.data.defaultAction).toEqual('REJECT')
+    })
+  })
+
+  describe('PUT /api/v1/rules', () => {
+    it('should require a valid user API key', async () => {
+      let res = await agent
+        .put(endpoint)
+        .query({ apiKey: user.apiKey })
+
+      expect(res.status).toBe(400)
+
+      res = await agent
+        .get(endpoint)
+        .query({ apiKey: 'invalid' })
+
+      expect(res.status).toBe(401)
+    })
+
+    it('should validate a host was specified', async () => {
+      const res = await agent
+        .put(endpoint)
+        .query({ apiKey: user.apiKey })
+        .send({
+          action: 'REJECT',
+          type: 'ACCESS'
+        })
+
+      expect(res.status).toBe(400)
+    })
+
+    it('should validate an action was specified', async () => {
+      const res = await agent
+        .put(endpoint)
+        .query({ apiKey: user.apiKey })
+        .send({
+          host: '*.google.com',
+          type: 'ACCESS'
+        })
+
+      expect(res.status).toBe(400)
+    })
+
+    it('should validate a rule type was specified', async () => {
+      const res = await agent
+        .put(endpoint)
+        .query({ apiKey: user.apiKey })
+        .send({
+          action: 'REJECT',
+          host: '*.google.com'
+        })
+
+      expect(res.status).toBe(400)
+    })
+
+    it('should add the rule to the rules collection', async () => {
+      const res = await agent
+        .put(endpoint)
+        .query({ apiKey: user.apiKey })
+        .send({
+          action: 'REJECT',
+          type: 'ACCESS',
+          host: '*.unit.test'
+        })
+
+      expect(res.status).toBe(200)
+      const count = await connection.database
+        .collection('rules')
+        .countDocuments({ host: '*.unit.test' })
+
+      expect(count).toBe(1)
+    })
+
+    it('should return the new rule object', async () => {
+      const res = await agent
+        .put(endpoint)
+        .query({ apiKey: user.apiKey })
+        .send({
+          action: 'REJECT',
+          type: 'ACCESS',
+          host: '*.unit.test'
+        })
+
+      const rule = await connection.database
+        .collection('rules')
+        .findOne({ host: '*.unit.test' })
+
+      expect(res.status).toBe(200)
+      expect(res.body.data).toEqual({
+        id: rule._id.toString(),
+        action: rule.action,
+        type: rule.type,
+        host: rule.host
+      })
+    })
+  })
+
+  describe('DELETE /api/v1/rules', () => {
+    it('should require a valid user API key', async () => {
+      let res = await agent
+        .delete(endpoint)
+        .query({ apiKey: user.apiKey })
+
+      expect(res.status).not.toBe(401)
+
+      res = await agent
+        .delete(endpoint)
+        .query({ apiKey: 'invalid' })
+
+      expect(res.status).toBe(401)
+    })
+
+    it('should validate a rule ID was specified', async () => {
+      const res = await agent
+        .delete(endpoint)
+        .query({ apiKey: user.apiKey })
+
+      expect(res.status).toBe(400)
+    })
+
+    it('should delete the specified rule', async () => {
+      const doc = await connection.database
+        .collection('rules')
+        .findOne({ host: 'rastating.github.io' })
+
+      const res = await agent
+        .delete(endpoint)
+        .query({
+          id: doc._id.toString(),
+          apiKey: user.apiKey
+        })
+
+      expect(res.status).toBe(200)
+
+      const count = await connection.database
+        .collection('rules')
+        .countDocuments({ host: 'rastating.github.io' })
+
+      expect(count).toBe(0)
+    })
+  })
+
+  describe('POST /api/v1/rules', () => {
+    it('should require a valid user API key', async () => {
+      let res = await agent
+        .post(endpoint)
+        .query({ apiKey: user.apiKey })
+
+      expect(res.status).not.toBe(401)
+
+      res = await agent
+        .delete(endpoint)
+        .query({ apiKey: 'invalid' })
+
+      expect(res.status).toBe(401)
+    })
+
+    describe('when an array of rules are in the body', () => {
+      it('should validate a host was specified for each rule', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send([{
+            id: rules[0].id,
+            type: 'ACCESS',
+            action: 'ALLOW',
+            host: 'rastating.github.io'
+          },
+          {
+            id: rules[1].id,
+            type: 'ACCESS',
+            action: 'ALLOW'
+          }])
+
+        expect(res.status).toBe(400)
+      })
+
+      it('should validate an action was specified for each rule', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send([{
+            id: rules[0].id,
+            type: 'ACCESS',
+            action: 'ALLOW',
+            host: 'rastating.github.io'
+          },
+          {
+            id: rules[1].id,
+            type: 'ACCESS',
+            host: 'google.com'
+          }])
+
+        expect(res.status).toBe(400)
+      })
+
+      it('should validate a rule type was specified for each rule', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send([{
+            id: rules[0].id,
+            type: 'ACCESS',
+            action: 'ALLOW',
+            host: 'rastating.github.io'
+          },
+          {
+            id: rules[1].id,
+            action: 'ALLOW',
+            host: 'google.com'
+          }])
+
+        expect(res.status).toBe(400)
+      })
+
+      it('should update the specified rules', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send([{
+            id: rules[0].id,
+            type: 'EDITED.TYPE.1',
+            action: 'EDITED.ACTION.1',
+            host: 'edited.1'
+          },
+          {
+            id: rules[1].id,
+            type: 'EDITED.TYPE.2',
+            action: 'EDITED.ACTION.2',
+            host: 'edited.2'
+          }])
+
+        expect(res.status).toBe(200)
+        let rule = await connection.database
+          .collection('rules')
+          .findOne({ _id: rules[0].id })
+
+        expect(rule.type).toEqual('EDITED.TYPE.1')
+        expect(rule.action).toEqual('EDITED.ACTION.1')
+        expect(rule.host).toEqual('edited.1')
+
+        rule = await connection.database
+          .collection('rules')
+          .findOne({ _id: rules[1].id })
+
+        expect(rule.type).toEqual('EDITED.TYPE.2')
+        expect(rule.action).toEqual('EDITED.ACTION.2')
+        expect(rule.host).toEqual('edited.2')
+      })
+
+      it('should return the updated rule objects', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send([{
+            id: rules[0].id,
+            type: 'EDITED.TYPE.1',
+            action: 'EDITED.ACTION.1',
+            host: 'edited.1'
+          },
+          {
+            id: rules[1].id,
+            type: 'EDITED.TYPE.2',
+            action: 'EDITED.ACTION.2',
+            host: 'edited.2'
+          }])
+
+        expect(res.status).toBe(200)
+        expect(res.body.data).toEqual([{
+          id: rules[0].id,
+          type: 'EDITED.TYPE.1',
+          action: 'EDITED.ACTION.1',
+          host: 'edited.1'
+        },
+        {
+          id: rules[1].id,
+          type: 'EDITED.TYPE.2',
+          action: 'EDITED.ACTION.2',
+          host: 'edited.2'
+        }])
+      })
+    })
+
+    describe('when a single rule is submitted', () => {
+      it('should validate a host was specified', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send({
+            id: rules[1].id,
+            type: 'ACCESS',
+            action: 'ALLOW'
+          })
+
+        expect(res.status).toBe(400)
+      })
+
+      it('should validate an action was specified', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send({
+            id: rules[1].id,
+            type: 'ACCESS',
+            host: 'google.com'
+          })
+
+        expect(res.status).toBe(400)
+      })
+
+      it('should validate a rule type was specified', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send({
+            id: rules[1].id,
+            action: 'ALLOW',
+            host: 'google.com'
+          })
+
+        expect(res.status).toBe(400)
+      })
+
+      it('should update the specified rule', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send({
+            id: rules[1].id,
+            type: 'EDITED.TYPE.2',
+            action: 'EDITED.ACTION.2',
+            host: 'edited.2'
+          })
+
+        expect(res.status).toBe(200)
+
+        const rule = await connection.database
+          .collection('rules')
+          .findOne({ _id: rules[1].id })
+
+        expect(rule.type).toEqual('EDITED.TYPE.2')
+        expect(rule.action).toEqual('EDITED.ACTION.2')
+        expect(rule.host).toEqual('edited.2')
+      })
+
+      it('should return the updated rule object', async () => {
+        const res = await agent
+          .post(endpoint)
+          .query({ apiKey: user.apiKey })
+          .send({
+            id: rules[1].id,
+            type: 'EDITED.TYPE.2',
+            action: 'EDITED.ACTION.2',
+            host: 'edited.2'
+          })
+
+        expect(res.status).toBe(200)
+        expect(res.body.data).toEqual([{
+          id: rules[1].id,
+          type: 'EDITED.TYPE.2',
+          action: 'EDITED.ACTION.2',
+          host: 'edited.2'
+        }])
+      })
+    })
+  })
+})
