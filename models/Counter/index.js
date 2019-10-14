e6e577f2b139090fb395535d6ea2b8cee449725b
Rob <robert.carr@owasp.org>
2019-10-15 00:32:33 +0100

    Add counter model

---
+++ models/Counter/index.js
@@ -0,0 +1,77 @@
+const { Connection } = require('>/lib/database')
+
+class Counter {
+  constructor () {
+    this.host = ''
+    this.allowed = 0
+    this.blocked = 0
+  }
+
+  static async find (host) {
+    const connection = await Connection.connect()
+    const doc = await connection
+      .database
+      .collection('counters')
+      .findOne({ host })
+
+    const counter = new Counter()
+    counter.host = host
+
+    if (doc != null) {
+      counter.allowed = doc.allowed
+      counter.blocked = doc.blocked
+    }
+
+    return counter
+  }
+
+  static async all ({ sort = { host: 1 }, limit = 0 } = {}) {
+    const counters = []
+    const connection = await Connection.connect()
+    const docs = await connection
+      .database
+      .collection('counters')
+      .find({
+        host: {
+          $ne: '<all>'
+        }
+      }, {
+        sort,
+        limit
+      })
+      .toArray()
+
+    for (let i = 0; i < docs.length; i++) {
+      const counter = new Counter()
+      counter.host = docs[i].host
+      counter.allowed = docs[i].allowed
+      counter.blocked = docs[i].blocked
+      counters.push(counter)
+    }
+
+    return counters
+  }
+
+  async save () {
+    const connection = await Connection.connect()
+
+    try {
+      await connection
+        .database
+        .collection('counters')
+        .updateOne({ host: this.host }, {
+          $set: {
+            host: this.host,
+            allowed: this.allowed,
+            blocked: this.blocked
+          }
+        }, { upsert: true })
+
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+}
+
+module.exports = Counter
---
+++ models/Counter/index.spec.js
@@ -0,0 +1,183 @@
+const { Connection } = require('>/lib/database')
+const { ObjectID } = require('mongodb')
+
+const Counter = require('./')
+
+describe('models/Counter', () => {
+  let connection
+
+  const createCounter = async (host, allowed, blocked) => {
+    await connection
+      .database
+      .collection('counters')
+      .insertOne({
+        host,
+        allowed,
+        blocked
+      })
+  }
+
+  beforeEach(async () => {
+    connection = await Connection.connect()
+    await connection.database.collection('counters').removeMany({})
+    await createCounter('<all>', 5, 10)
+    await createCounter('rastating.github.io', 1337, 0)
+    await createCounter('google.com', 0, 20)
+  })
+
+  afterAll(async () => {
+    await connection.database.collection('counters').removeMany({})
+    await connection.client.close()
+  })
+
+  describe('.find', () => {
+    it('should return the global counter when the `criteria` is `<all>`', async () => {
+      const res = await Counter.find('<all>')
+      expect(res.host).toEqual('<all>')
+      expect(res.allowed).toEqual(5)
+      expect(res.blocked).toEqual(10)
+    })
+
+    it('should return the counter for the specified domain in `criteria`', async () => {
+      const res = await Counter.find('rastating.github.io')
+      expect(res.host).toEqual('rastating.github.io')
+      expect(res.allowed).toEqual(1337)
+      expect(res.blocked).toEqual(0)
+    })
+
+    describe('if the domain has no counter in the database', () => {
+      it('should return a `Counter` with the values set to `0`', async () => {
+        const res = await Counter.find('new.host')
+        expect(res.host).toEqual('new.host')
+        expect(res.allowed).toEqual(0)
+        expect(res.blocked).toEqual(0)
+      })
+    })
+
+    it('should populate the `host` property', async () => {
+      const res = await Counter.find('rastating.github.io')
+      expect(res.host).toEqual('rastating.github.io')
+    })
+
+    it('should populate the `allowed` property', async () => {
+      const res = await Counter.find('rastating.github.io')
+      expect(res.allowed).toEqual(1337)
+    })
+
+    it('should populate the `blocked` property', async () => {
+      const res = await Counter.find('rastating.github.io')
+      expect(res.blocked).toEqual(0)
+    })
+  })
+
+  describe('.all', () => {
+    it('should return an array of `Counter` objects (excluding the global counter)', async () => {
+      const res = await Counter.all()
+      expect(res).toHaveLength(2)
+    })
+
+    it('should populate the `host` property of each object', async () => {
+      const res = await Counter.all()
+      for (const counter of res) {
+        expect(counter.host).not.toBeUndefined()
+      }
+    })
+
+    it('should populate the `allowed` property of each object', async () => {
+      const res = await Counter.all()
+      for (const counter of res) {
+        expect(counter.allowed).not.toBeUndefined()
+      }
+    })
+
+    it('should populate the `blocked` property of each object', async () => {
+      const res = await Counter.all()
+      for (const counter of res) {
+        expect(counter.blocked).not.toBeUndefined()
+      }
+    })
+
+    it('should allow a custom sort to be defined', async () => {
+      let res = await Counter.all({
+        sort: { allowed: -1 }
+      })
+
+      expect(res[0].host).toEqual('rastating.github.io')
+      expect(res[1].host).toEqual('google.com')
+
+      res = await Counter.all({
+        sort: { allowed: 1 }
+      })
+
+      expect(res[1].host).toEqual('rastating.github.io')
+      expect(res[0].host).toEqual('google.com')
+    })
+
+    it('should allow a custom limit to be defined', async () => {
+      const res = await Counter.all({
+        sort: { allowed: -1 },
+        limit: 1
+      })
+
+      expect(res[0].host).toEqual('rastating.github.io')
+      expect(res).toHaveLength(1)
+    })
+  })
+
+  describe('#save', () => {
+    describe('when the host does not exist', () => {
+      it('should insert a new document', async () => {
+        const res = await Counter.find('new.host')
+        const count = await connection
+          .database
+          .collection('counters')
+          .countDocuments({ host: 'new.host' })
+
+        expect(count).toBe(0)
+
+        res.allowed = 1
+        res.blocked = 2
+        await res.save()
+
+        const doc = await connection
+          .database
+          .collection('counters')
+          .findOne({ host: 'new.host' })
+
+        expect(doc.allowed).toBe(1)
+        expect(doc.blocked).toBe(2)
+      })
+    })
+
+    describe('when the host exists in the database', () => {
+      it('should update the existing document', async () => {
+        const res = await Counter.find('rastating.github.io')
+        let count = await connection
+          .database
+          .collection('counters')
+          .countDocuments({ host: 'rastating.github.io' })
+
+        expect(count).toBe(1)
+
+        res.allowed = 1001
+        res.blocked = 1002
+        await res.save()
+
+        count = await connection
+          .database
+          .collection('counters')
+          .countDocuments({ host: 'rastating.github.io' })
+
+        expect(count).toBe(1)
+
+        const doc = await connection
+          .database
+          .collection('counters')
+          .findOne({ host: 'rastating.github.io' })
+
+        expect(doc.allowed).toBe(1001)
+        expect(doc.blocked).toBe(1002)
+      })
+    })
+  })
+})
